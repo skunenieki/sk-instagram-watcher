1b0a5facc5f6519bdb0b55072f5bd954d03e3cab
Martins Sipenko <martins.sipenko@accenture.com>
2014-07-18 19:38:14 +0000

    Revert "more logging"
    This reverts commit 414d7182463609c0b142b5d2248d57b0028b738a.

--- MyProcessor.php
+++ MyProcessor.php
@@ -1,7 +1,9 @@
 <?php
 
 class MyProcessor extends SubscriptionProcessor {
+	const client_secret = 'YOUR API SECRET';
+	// Redefine this function
 	public static function process($data){
-            error_log(json_encode($data));
+            error_log($data);
 	}
 }
--- index.php
+++ index.php
@@ -6,14 +6,13 @@ require_once 'MyProcessor.php';
 $igKey    = getenv('INSTAGRAM_CLIENT_ID');
 $igSecret = getenv('INSTAGRAM_CLIENT_SECRET');
 
-error_log(json_encode($_POST));
-error_log(json_encode($_GET));
-
 if (isset($_GET['hub_challenge'])) {
     echo $_GET['hub_challenge'];
-} elseif (isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+} elseif( isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
     $igdata = file_get_contents("php://input");
-    MyProcessor::process(json_decode($igdata));
+    if (hash_hmac('sha1', $igdata, MyProcessor::client_secret) == $_SERVER['HTTP_X_HUB_SIGNATURE']) {
+        MyProcessor::process(json_decode($igdata));
+    }
 } else {
     $irt = new InstagramRealTime($igKey, $igSecret, 'http://sk-instagram-watcher.herokuapp.com/');
     $r = $irt->addSubscription('tag', 'media', 'skunenieki');
