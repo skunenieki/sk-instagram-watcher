414d7182463609c0b142b5d2248d57b0028b738a
Martins Sipenko <martins.sipenko@accenture.com>
2014-07-18 19:31:05 +0000

    more logging

--- MyProcessor.php
+++ MyProcessor.php
@@ -1,9 +1,7 @@
 <?php
 
 class MyProcessor extends SubscriptionProcessor {
-	const client_secret = 'YOUR API SECRET';
-	// Redefine this function
 	public static function process($data){
-            error_log($data);
+            error_log(json_encode($data));
 	}
 }
